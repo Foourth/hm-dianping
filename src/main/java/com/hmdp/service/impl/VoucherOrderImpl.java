df64bd1b0430efdc7d9edd44c7c9f0993ad9ea66
十四 <1269921156@qq.com>
2022-09-29 16:46:35 +0800

    add 优惠券秒杀

---
+++ src/main/java/com/hmdp/service/impl/VoucherOrderImpl.java
@@ -0,0 +1,63 @@
+package com.hmdp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.Voucher;
+import com.hmdp.entity.VoucherOrder;
+import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
+import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
+@Service
+public class VoucherOrderImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+
+
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1  查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2 判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始");
+        }
+
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已结束");
+        }
+        // 3 判断是否有库存
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足");
+        }
+        // 4 减库存
+        seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).update();
+        // 5 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        return Result.ok(orderId);
+    }
+}
